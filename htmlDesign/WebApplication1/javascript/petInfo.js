3ef73799a563b4041f64a17b1567b59f87ef92c6
joe850715 <barinhow850715@gmail.com>
2021-09-17 16:25:15 +0800

    1.將img資料夾放入image資料夾中 2.新增font資料夾 3.新增petInfo.html、css、js檔案

---
+++ htmlDesign/WebApplication1/javascript/petInfo.js
@@ -0,0 +1,15 @@
+// JavaScript source code
+$(function () {
+    window.onscroll = function () { stickTop() };
+    var topbar = document.getElementById("topbar");
+    var distance = topbar.offsetTop; //取得topbar上方偏移量
+
+    function stickTop() {
+        if (window.pageYOffset >= distance) { //以頁面Y軸偏移量為判斷條件
+            topbar.classList.add("sticky");
+        } else {
+            topbar.classList.remove("sticky");
+        }
+    }
+
+});
