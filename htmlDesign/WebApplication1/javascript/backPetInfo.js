66b3f9764580f4f2d06c53264c64eb6533a10f82
joe850715 <barinhow850715@gmail.com>
2021-09-21 23:56:17 +0800

    1.將寵物資訊管理後台更新頁面更改為modal形式 2.petInfoAdd系列的html/css/js檔案暫時沒有用處純粹留在裡面而已

--- htmlDesign/WebApplication1/javascript/backPetInfo.js
+++ htmlDesign/WebApplication1/javascript/backPetInfo.js
@@ -1,4 +1,13 @@
 // JavaScript source code
-function openEdit() {
+$(function () {
+    //日期選擇器
+    $("input[name='adoptDate']").datepicker({
+        dateFormat: 'yy/mm/dd',
+        changeYear: true,
+        changeMonth: true
+    });
+});
+
+/*function openEdit() {
     window.open("petInfoAdd.html", "EditWindow", "height=600,width=800,top=200,left=600");
-}
+}*/
