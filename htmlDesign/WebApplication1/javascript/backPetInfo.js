2bc5b1d45fb53b0635ccee92967a2b7d4b3b5366
joe850715 <barinhow850715@gmail.com>
2021-09-22 00:32:13 +0800

    新增後台刪除資料方法，讓刪除按鈕確實具備刪除功能

--- htmlDesign/WebApplication1/javascript/backPetInfo.js
+++ htmlDesign/WebApplication1/javascript/backPetInfo.js
@@ -1,12 +1,20 @@
 // JavaScript source code
 $(function () {
+
     //日期選擇器
     $("input[name='adoptDate']").datepicker({
         dateFormat: 'yy/mm/dd',
         changeYear: true,
         changeMonth: true
     });
+
+
 });
+
+//刪除資料事件
+function del(obj) {
+    $(obj).parents("tr").remove();
+}
 
 /*function openEdit() {
     window.open("petInfoAdd.html", "EditWindow", "height=600,width=800,top=200,left=600");
