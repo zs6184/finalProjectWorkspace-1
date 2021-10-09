5db6d65d1469951b82f52b5515f46b038bb35297
iiiJinChao <q2469181q@gmail.com>
2021-10-10 02:49:18 +0800

    20211010

---
+++ workspaceTest/NoXmlSpringMvcProject/src/main/java/tw/finalspring/model/Coupons.java
@@ -0,0 +1,68 @@
+package tw.finalspring.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.stereotype.Component;
+
+@Entity
+@Table(name = "[Coupons]")
+@Component
+public class Coupons {
+	//Àu´f½XID
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "coupon_ID")
+	private int couponId;
+	//Àu´f½X¦WºÙ
+	@Column(name = "coupon_name")
+	private String couponName;
+	//Àu´f½X
+	@Column(name = "coupon_code")
+	private String couponCode;
+	//Àu´f½X§é»ùª÷ÃB
+	@Column(name = "[discount]")
+	private int	couponDiscount;
+	//Àu´f½X¹L´Á¤é
+	@Column(name = "due_date")
+	private Timestamp couponDue;
+	
+	public int getCouponId() {
+		return couponId;
+	}
+	public void setCouponId(int couponId) {
+		this.couponId = couponId;
+	}
+	public String getCouponName() {
+		return couponName;
+	}
+	public void setCouponName(String couponName) {
+		this.couponName = couponName;
+	}
+	public String getCouponCode() {
+		return couponCode;
+	}
+	public void setCouponCode(String couponCode) {
+		this.couponCode = couponCode;
+	}
+	public int getCouponDiscount() {
+		return couponDiscount;
+	}
+	public void setCouponDiscount(int couponDiscount) {
+		this.couponDiscount = couponDiscount;
+	}
+	public Timestamp getCouponDue() {
+		return couponDue;
+	}
+	public void setCouponDue(Timestamp couponDue) {
+		this.couponDue = couponDue;
+	}
+	
+
+}
