7742dc16b679cceae7f8c4a32c6a19274634207c
iiiJinChao <q2469181q@gmail.com>
2021-10-12 23:42:23 +0800

    刪除

--- workspaceTest/NoXmlSpringMvcProject/src/main/webapp/WEB-INF/pages/javascript/petInfoAdd.js
+++
@@ -1,9 +0,0 @@
-// JavaScript source code
-$(function () {
-    //日期選擇器
-    $("input[name='adoptDate']").datepicker({
-        dateFormat: 'yy/mm/dd',
-        changeYear: true,
-        changeMonth: true
-    });
-});
