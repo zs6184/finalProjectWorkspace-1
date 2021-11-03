f8a8f6df6397f0c3cc88a64c1eed8aaeb085e539
joe850715 <barinhow850715@gmail.com>
2021-11-03 23:23:33 +0800

    for merge

--- workspaceTest/SpringBootFinalProject/src/main/java/tw/springbootfinal/users/controller/LoginController.java
+++ workspaceTest/SpringBootFinalProject/src/main/java/tw/springbootfinal/users/controller/LoginController.java
@@ -15,11 +15,13 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttribute;
