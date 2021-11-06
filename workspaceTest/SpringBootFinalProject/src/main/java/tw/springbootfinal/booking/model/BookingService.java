b97800afe108d4b85c8ad6e07939af00931d208e
Simpleyi <ww252046@gmail.com>
2021-11-06 21:18:41 +0800

    訂位會員中心前台頁面........ (( 失約按鈕先放著..QQ

--- workspaceTest/SpringBootFinalProject/src/main/java/tw/springbootfinal/booking/model/BookingService.java
+++ workspaceTest/SpringBootFinalProject/src/main/java/tw/springbootfinal/booking/model/BookingService.java
@@ -2,10 +2,15 @@ package tw.springbootfinal.booking.model;
 
 import java.util.List;
 
+import tw.springbootfinal.reservation.model.AdoptReservation;
+
 public interface BookingService {
     BookingsBean getById(int id);
     List<BookingsBean> getAll();
-
+    //回傳使用者ID給個人紀錄頁面
+    List<BookingsBean> selectTheCusRec(Integer id);
+    
+    
     // 此為避免model直接對外使用
     void save(BookingsDTO bookingsDTO);
     void save(BookingsBean bookingsBean);
